61b9fb8b1ebe934322412ca9208b25f75d3a111c
mystist <gjl87910lq@gmail.com>
2016-08-30 14:21:48 +0800

    :tada: Rough static markups

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 var db = require('../components/db');
 
-router.get('/feeds', function (req, res, next) {
-  db.getAllFeedsAsync().then(function (data) {
+router.get('/feeds/:date', function (req, res, next) {
+  var date = req.params.date;
+  var currentDateString = moment.utc(date).utcOffset('+0800').format('YYYYMMDD');
+  var eTimestamp = moment.utc(currentDateString).add(-8, 'hours').valueOf();
+  var sTimestamp = moment.utc(eTimestamp).add(-1, 'day').valueOf();
+
+  db.getFeedsBetweenTimestampsAsync(sTimestamp, eTimestamp).then(function (data) {
     res.json(data);
   });
 });
